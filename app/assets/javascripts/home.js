71793e50d16665e5bd96c3a8fc7c96dc84b3373e
Callum Tan <mullacyo@hotmail.com>
2019-01-25 11:15:44 +0800

    Added AJAX to the searchbox

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,61 +1,61 @@
-var city_input = document.getElementById("query")
+// var city_input = document.getElementById("query")
 
-if (city_input){
-  city_input.addEventListener("keyup", function(event){
-    autocomplete(event)
-  })
-}
+// if (city_input){
+//   city_input.addEventListener("keyup", function(event){
+//     autocomplete(event)
+//   })
+// }
 
-window.globalXHR = new XMLHttpRequest()
+// window.globalXHR = new XMLHttpRequest()
 
-function autocomplete(event) {
-  //retrieve input
-  var input = event.target
+// function autocomplete(event) {
+//   //retrieve input
+//   var input = event.target
 
-  //get datalist element for results
-  var list = document.getElementById("list")
+//   //get datalist element for results
+//   var list = document.getElementById("list")
 
-  //set minimum num of chars
-  var min_chars = 0
+//   //set minimum num of chars
+//   var min_chars = 0
 
-  if (input.value.length > min_chars) {
-    //abort pending requests
-    //ensure that our XHR object is not working on any pending request before we tell it to do new work
-    window.globalXHR.abort()
+//   if (input.value.length > min_chars) {
+//     //abort pending requests
+//     //ensure that our XHR object is not working on any pending request before we tell it to do new work
+//     window.globalXHR.abort()
 
-    // Callback to be applied once the XMLHttpRequest() internal state has been changed after
-    // sending a request
-    // We check to see whether the request is done and that the return status of the request is ok
-    window.globalXHR.onreadystatechange = function(){
-      if (this.readyState == 4 && this.status == 200) {
+//     // Callback to be applied once the XMLHttpRequest() internal state has been changed after
+//     // sending a request
+//     // We check to see whether the request is done and that the return status of the request is ok
+//     window.globalXHR.onreadystatechange = function(){
+//       if (this.readyState == 4 && this.status == 200) {
         
-        //convert json response to an object
-        var response = JSON.parse(this.responseText)
+//         //convert json response to an object
+//         var response = JSON.parse(this.responseText)
 
-        //clear previous results
-        list.innerHTML = ""
+//         //clear previous results
+//         list.innerHTML = ""
 
-        response.forEach(function(element){
-          //create new option element
-          var option = document.createElement("option")
-          option.value = element
+//         response.forEach(function(element){
+//           //create new option element
+//           var option = document.createElement("option")
+//           option.value = element
 
-          //append option to list
-          list.appendChild(option)
-        })
-      }
-    }
+//           //append option to list
+//           list.appendChild(option)
+//         })
+//       }
+//     }
 
-    // authentication is required whenever our client wishes to make a request to our server
-    // this is to prevent csrf attacks on our servers
-    // more info can be found at the link below
-    // https://www.checkmarx.com/2016/01/22/ultimate-guide-understanding-preventing-csrf/
-    var auth_token = document.querySelector("[name='csrf-token']").content
-    window.globalXHR.open("POST", "/search?query=" + input.value, true)
-    //allow js to make request to rails server
-    window.globalXHR.setRequestHeader("Accept", "application/json");
-    window.globalXHR.setRequestHeader("X-CSRF-TOKEN", auth_token)
-    window.globalXHR.send()
-  }
-}
+//     // authentication is required whenever our client wishes to make a request to our server
+//     // this is to prevent csrf attacks on our servers
+//     // more info can be found at the link below
+//     // https://www.checkmarx.com/2016/01/22/ultimate-guide-understanding-preventing-csrf/
+//     var auth_token = document.querySelector("[name='csrf-token']").content
+//     window.globalXHR.open("POST", "/search?query=" + input.value, true)
+//     //allow js to make request to rails server
+//     window.globalXHR.setRequestHeader("Accept", "application/json");
+//     window.globalXHR.setRequestHeader("X-CSRF-TOKEN", auth_token)
+//     window.globalXHR.send()
+//   }
+// }
 
--- app/assets/javascripts/home_ajax.js
+++ app/assets/javascripts/home_ajax.js
@@ -1,26 +1,29 @@
-// $("#city-search").on("keyup", function(){
-//   autocomplete()
-// });
+$("#city-search").on("keyup", function(){
+  autocomplete()
+});
 
-// function autocomplete(){
-//   $.ajax({
-//     url: '/search',
-//     type: 'POST',
-//     data: $("#city-search").serialize(), //takes the form data and authenticity toke and converts it into a standard URL
-//     dataType: 'json', //specify what type of data you're expecting back from the servers
-//     error: function() {
-//         console.log("Something went wrong");
-//     },
-//     success: function(data) {
-//       $("#list").html("");
+function autocomplete(){
+  $.ajax({
+    url: '/search',
+    type: 'POST',
+    data: $("#city-search").serialize(), //takes the form data and authenticity toke and converts it into a standard URL
+    dataType: 'json', //specify what type of data you're expecting back from the servers
+    error: function() {
+        console.log("Something went wrong");
+    },
+    success: function(data) {
+      $("#list").html("");
 
-//       data.forEach(function(element) {
-//         var option = document.createElement("option");
-//         option.value = element;
+      data.forEach(function(element) {
+        var option = document.createElement("option");
+        option.value = element;
 
-//         //append option to list
-//         $("#list").append(option);
-//       });
-//     }
-//   });
-// }
+        //append option to list
+        if ($('option').length < 10) {
+            $("#list").append(option);
+        }
+        
+      });
+    }
+  });
+}
